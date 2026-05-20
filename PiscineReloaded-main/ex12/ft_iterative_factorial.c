a06d139f35035745c3fcdb04ebb33e89e7f722dc
Millena Matos <matosmillena015@gmail.com>
2026-05-20 00:20:51 -0300

    envia exercicios

---
+++ PiscineReloaded-main/ex12/ft_iterative_factorial.c
@@ -0,0 +1,16 @@
+int ft_iterative_factorial(int nb);
+
+int ft_iterative_factorial(int nb)
+{
+    int result;
+
+    if (nb < 0)
+        return (0);
+    result = 1;
+    while (nb > 1)
+    {
+        result = result * nb;
+        nb--;
+    }
+    return (result);
+}
---
+++ PiscineReloaded-main/ex14/ft_sqrt.c
@@ -0,0 +1,17 @@
+int ft_sqrt(int nb);
+
+int ft_sqrt(int nb)
+{
+    int i;
+
+    i = 1;
+    while (i * i <= nb)
+    {
+        if (i * i == nb)
+        {
+            return (i);
+        }
+        i++;
+    }
+    return (0);
+}
